6b6f9319d1d05083c8f3ba0d86bc92e6f4efa96c
Patrik Sundblom <89919658+iampathat@users.noreply.github.com>
2026-08-31 13:27:59 +0200

    BUILD 93: publish threadless Central Fabric fix

--- src/qcds_fabric/living_robot_public.py
+++ src/qcds_fabric/living_robot_public.py
@@ -8,7 +8,7 @@ from typing import Sequence
 from .living_robot_public_visual87 import living_robot_public_visual87_html as _base_html
 
 
-PUBLIC_BUILD = "90"
+PUBLIC_BUILD = "93"
 
 _FACTS_CSS = r'''
 /* BUILD 65: playground facts are metadata, not action cards. */
